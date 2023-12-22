3f63c7b70612f3b8d68b10263d10d7cf9f641d2f
Ben Donet <brjd345@gmail.com>
2023-12-22 09:29:32 -0600

    Appended FirstTimeSetup to run profile script

--- FirstTimeSetup.ps1
+++ FirstTimeSetup.ps1
@@ -4,4 +4,6 @@ dotnet tool install --global dotnet-ef
 
 Copy-Item .\Microsoft.PowerShell_profile.ps1 -Destination "$HOME\Documents\WindowsPowerShell"
 Copy-Item .\Microsoft.PowerShell_profile.ps1 -Destination "$HOME\Documents\PowerShell"
+
+.\Microsoft.PowerShell_profile.ps1
 exit
