1fabd61f7413057fcaad9e3d14c659ac340d2367
Ben Donet <brjd345@gmail.com>
2023-12-22 09:27:47 -0600

    Appended FirstTimeSetup to copy initial powershell profile to profile location

--- FirstTimeSetup.ps1
+++ FirstTimeSetup.ps1
@@ -1,4 +1,7 @@
 PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 winget install JanDeDobbeleer.OhMyPosh -s winget
 dotnet tool install --global dotnet-ef
+
+Copy-Item .\Microsoft.PowerShell_profile.ps1 -Destination "$HOME\Documents\WindowsPowerShell"
+Copy-Item .\Microsoft.PowerShell_profile.ps1 -Destination "$HOME\Documents\PowerShell"
 exit
