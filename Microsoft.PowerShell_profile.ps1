067a75a65c68bf30610fdfef18096d87ceebb600
Ben Donet <bedonet@ttu.edu>
2024-01-02 08:30:18 -0600

    Changed csc name for clarity
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,12 +3,12 @@ $repos = "C:\Repos"
 $personaltools = "$repos\Tools"
 $keymaps = "$repos\QMKKeymaps"
 $autotrader = "$repos\Auto-Trader"
-$csc = "$repos\CSC"
+$ttucsc = "$repos\CSC"
 $ttuhrms = "$repos\HRMS"
 Function tools {Set-Location $personaltools}
 Function qmk {Set-Location $keymaps}
 Function trader {Set-Location $autotrader}
-Function csc {Set-Location $csc}
+Function csc {Set-Location $ttucsc}
 Function hrms {Set-Location $ttuhrms}
 
 # Open various applications
@@ -141,7 +141,7 @@ Import-Module posh-git
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-Expression
 
 setupRepos $repos
-setupGit $csc
+setupGit $ttucsc
 setupGit $ttuhrms
 
 Set-PSReadlineOption -Colors @{ String = '#c69ee6'}
