81347ee27881c459096071875a58e2f01c17aa45
Ben Donet <bedonet@ttu.edu>
2025-03-31 16:38:47 -0500

    Added shortcut to CSC repo
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,11 @@ $repos = "C:\Repos"
 $personaltools = "$repos\Tools"
 $keymaps = "$repos\QMKKeymaps"
 $autotrader = "$repos\Auto-Trader"
+$csc = "$repos\CSC"
 Function tools {Set-Location $personaltools}
 Function qmk {Set-Location $keymaps}
 Function trader {Set-Location $autotrader}
+Function csc {Set-Location $csc}
 
 # Open various applications
 $vs = "C:\Program Files\Microsoft Visual Studio\2022\Professional"
