ba8efb7840fbb8586788d0fcf97aafc4e3d3c886
Ben Donet <bedonet@ttu.edu>
2025-03-31 16:39:26 -0500

    Added shortcut to Genetec repo
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1
    
    # Conflicts:
    #	Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,11 +5,13 @@ $keymaps = "$repos\QMKKeymaps"
 $autotrader = "$repos\Auto-Trader"
 $ttucsc = "$repos\CSC"
 $ttuhrms = "$repos\HRMS"
+$ttugenetec = "$repos\GenetecAPI"
 Function tools {Set-Location $personaltools}
 Function qmk {Set-Location $keymaps}
 Function trader {Set-Location $autotrader}
 Function csc {Set-Location $ttucsc}
 Function hrms {Set-Location $ttuhrms}
+Function gtec {Set-Location $ttugenetec}
 
 # Open various applications
 $vs = "C:\Program Files\Microsoft Visual Studio\2022\Professional"
